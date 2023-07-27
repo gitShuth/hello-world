0f6e01f8dd3fa11268911d991e6614684c560e20
shutianhao <862333966@qq.com>
2023-07-27 09:39:25 +0800

    格式更改

--- src/layout/components/AppAside.vue
+++ src/layout/components/AppAside.vue
@@ -2,34 +2,34 @@
   <el-menu
     :default-active="selected"
     class="el-menu-vertical-demo"
-    :router="true"
-  >
+    :router="true">
     <menuItem
       v-for="(item, index) in menuList"
       :data="item"
-      :key="index"
-    ></menuItem>
+      :key="index"></menuItem>
   </el-menu>
 </template>
 
 <script lang="ts" setup>
-import { ref, watch, computed } from "vue";
-import { useRouter, useRoute, RouteRecordRaw } from "vue-router";
-import menuItem from "./menuItem.vue";
-import { MenuData } from "./MenuData";
+import { ref, watch, computed } from 'vue'
+import { useRouter, useRoute, RouteRecordRaw } from 'vue-router'
+import menuItem from './menuItem.vue'
+import { MenuData } from './MenuData'
 
-const router = useRouter();
-const route = useRoute();
-const menuList = ref<MenuData[]>([]);
-const selected = ref<string>('');
+const router = useRouter()
+const route = useRoute()
+const menuList = ref<MenuData[]>([])
+const selected = ref<string>('')
 
-const routes = router.getRoutes().find((item) => item.name === 'Index')?.children;
+const routes = router
+  .getRoutes()
+  .find((item) => item.name === 'Index')?.children
 
 const buildNav = (routes: RouteRecordRaw[] = []): MenuData[] => {
-  const result: MenuData[] = [];
+  const result: MenuData[] = []
   for (const route of routes) {
-    const { path, name, meta = {}, children = [] } = route;
-    const { icon, iconName, menu, subMenu, title } = meta;
+    const { path, name, meta = {}, children = [] } = route
+    const { icon, iconName, menu, subMenu, title } = meta
     if (menu) {
       result.push({
         routePath: path,
@@ -38,18 +38,18 @@ const buildNav = (routes: RouteRecordRaw[] = []): MenuData[] => {
         routeTitle: title as string,
         routeIcon: icon as boolean,
         IconName: iconName as string,
-        children: buildNav(children),
-      });
+        children: buildNav(children)
+      })
     }
   }
-  return result;
-};
-menuList.value = buildNav(routes);
+  return result
+}
+menuList.value = buildNav(routes)
 
 function findRoute(menu: MenuData[], Fn: Function) {
-  const routeArr = route.path.split('/').filter((val) => val !== '');
+  const routeArr = route.path.split('/').filter((val) => val !== '')
   for (let i: number = 0; i < routeArr.length; i++) {
-    if (Fn(menu, routeArr[i])) return;
+    if (Fn(menu, routeArr[i])) return
   }
 }
 watch(
@@ -57,19 +57,19 @@ watch(
   () => findRoute(menuList.value, findFn),
   {
     deep: true,
-    immediate: true,
+    immediate: true
   }
-);
+)
 
 function findFn(menu: MenuData[], routePath: string) {
   menu.forEach((item) => {
     if (item.routePath === routePath && item.subMenu === false) {
-      selected.value = item.routePath;
-      return true;
+      selected.value = item.routePath
+      return true
     } else {
-      findFn(item.children, routePath);
+      findFn(item.children, routePath)
     }
-  });
+  })
 }
 </script>
 
